1a6b803d3740bef4f167740ef8d3ab926cc177a3
mrobertsanchez <32586644+m@users.noreply.github.com>
2018-01-03 20:33:40 -0800

    updates

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+var enemy = function(x, y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //x, y coordinates
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -21,15 +21,21 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function() {
+  // Now write your own player class
+  // This class requires an update(), render() and
+  // a handleInput() method.
+  this.sprite = 'images/char-boy.png'
+  this.x = 200;
+  this.y = 400;
+}
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
